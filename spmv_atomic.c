78c5818111a3477a25aa13d009063c89554db6f7
ashish-17 <ashishj1707@gmail.com>
2017-02-18 14:36:43 -0500

    Input matrix files Fix large files from git

--- spmv_atomic.c
+++ spmv_atomic.c
@@ -1,21 +1,57 @@
 #include "genresult.cuh"
 #include <sys/time.h>
 
-__global__ void getMulAtomic_kernel(/*Arguments*/){
-    /* This is just an example empty kernel, you don't have to use the same kernel
- * name*/
+__global__ void getMulAtomic_kernel(const int nz, const int *rIndex, const int *cIndex, const float *val, const float *vec, float *res) {
+
+	int threadId = blockDim.x * blockIdx.x + threadIdx.x;
+	int threadCount = blockDim.x * gridDim.x;
+	int iter = nz % threadCount ? nz / threadCount + 1 : nz / threadCount;
+
+	for (int i = 0; i < iter; ++i) {
+		int dataId = threadId + i*threadCount;
+		if (dataId < nz) {
+			float data = val[dataId];
+			int r = rIndex[dataId];
+			int c = cIndex[dataId];
+			float tmp = data * vec[c];
+			atomicAdd(&res[r], tmp);
+		}
+	}
 }
 
 void getMulAtomic(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int blockSize, int blockNum){
-    /*Allocate here...*/
+	/*Allocate here...*/
 
-		/* Sample timing code */
-    struct timespec start, end;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    /*Invoke kernels...*/
+	int *d_cIndex, *d_rIndex;
+	float *d_val, *d_vec, *d_res;
+	cudaMalloc((void **)&d_cIndex, sizeof(int)*mat->nz);
+	cudaMalloc((void **)&d_rIndex, sizeof(int)*mat->nz);
+	cudaMalloc((void **)&d_val, sizeof(float)*mat->nz);
+	cudaMalloc((void **)&d_vec, sizeof(float)*vec->nz);
+	cudaMalloc((void **)&d_res, sizeof(float)*mat->M);
 
-    cudaDeviceSynchronize();
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    printf("Atomic Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
-    /*Deallocate.*/
+	cudaMemcpy(d_cIndex, mat->cIndex, mat->nz*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_rIndex, mat->rIndex, mat->nz*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_val, mat->val, mat->nz*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_vec, vec->val, vec->nz*sizeof(float), cudaMemcpyHostToDevice);
+
+	/* Sample timing code */
+	struct timespec start, end;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
+	/*Invoke kernels...*/
+	getMulAtomic_kernel<<<blockNum, blockSize>>>(mat->nz, d_rIndex, d_cIndex, d_val, d_vec, d_res);
+	cudaDeviceSynchronize();
+
+	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+	printf("Atomic Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
+
+	/*Deallocate.*/
+	cudaMemcpy(res->val, d_res, res->nz*sizeof(float), cudaMemcpyDeviceToHost);
+
+	cudaFree(d_cIndex);
+	cudaFree(d_rIndex);
+	cudaFree(d_val);
+	cudaFree(d_vec);
+	cudaFree(d_res);
 }
