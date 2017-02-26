ed56794d766040019302851e4673ea8ae8f21ef1
ashish-17 <ashishj1707@gmail.com>
2017-02-26 14:14:33 -0500

    Read me file and some minor changes

--- spmv_design.c
+++ spmv_design.c
@@ -157,7 +157,7 @@ void getMulDesign(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int bloc
 	cudaDeviceSynchronize();
 
 	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-	printf("Atomic Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
+	printf("Design Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
 
 	/*Deallocate.*/
 	cudaMemcpy(res->val, d_res, res->nz*sizeof(float), cudaMemcpyDeviceToHost);
--- spmv_segment.c
+++ spmv_segment.c
@@ -173,7 +173,7 @@ void getMulScan(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int blockS
 	cudaDeviceSynchronize();
 
 	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-	printf("Atomic Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
+	printf("Segment scan Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
 
 	/*Deallocate.*/
 	cudaMemcpy(res->val, d_res, res->nz*sizeof(float), cudaMemcpyDeviceToHost);
